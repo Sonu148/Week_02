4083f1934c428455a6e4763b049652e39717ef02
Sonu148 <sonukumar31815@gmail.com>
2025-01-18 16:56:09 +0530

    add circle problem

---
+++ Level_1/Circle/Circle.java
@@ -0,0 +1,26 @@
+package Day_02.Level_1.Circle;
+
+public class Circle {
+    private double radius;
+    private double area;
+
+    //Default Constructor.
+    Circle(){
+        this(1.0);
+    }
+
+    // Parameterised Constructor.
+    Circle(double radius){
+       this.radius=radius;
+    }
+
+    // calculate the area of the circle.
+    double calculateAreaOfCircle(){
+         area= 3.14*radius*radius;
+         return area;
+    }
+    void displayCircleArea(){
+        calculateAreaOfCircle();
+        System.out.println(area);
+     }
+}
---
+++ Level_1/Circle/CircleMain.java
@@ -0,0 +1,16 @@
+package Day_02.Level_1.Circle;
+
+public class CircleMain {
+    public static void main(String[ ]args){
+    
+        //calling the deafult constructor
+        Circle deafultCircle= new Circle();
+        System.out.println("The area  of the circle of default circle is: ");
+        deafultCircle.displayCircleArea();
+
+        // calling the parameterised constructor.
+        Circle customCircle= new Circle(5.0);
+        System.out.println("The area of the custom circle is:");
+        customCircle.displayCircleArea();
+    }
+}
